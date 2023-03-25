6001b5fa3a66b22e05f92186f49b9ec289c8ad51
Anastasia Belogolovtseva <somova_as@mail.ru>
2023-03-25 20:48:17 +0300

    create project

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return getNumber();
+            }
+        };
+    }
+
+    public int getNumber() {
+        return random.nextInt(max - min) + min;
+    }
+}
